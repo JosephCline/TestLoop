6bbf419e5eefe756f43aec4d82c0695d967a1926
Joseph Cline <JoeyCline101@gmail.com>
2018-10-15 08:51:29 -0400

    Create Loop.py

---
+++ Loop.py
@@ -0,0 +1,7 @@
+colors = ["green","brown","pink","blue","red"]
+counter = 0
+index = len(colors) - 1
+while  counter <= index:
+    text = colors[counter]
+    print(text + "!")
+    counter = counter + 1
